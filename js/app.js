4fac3513ea9dafe7a47075e54ea6d0f54432faba
Haskman <sahinmehmetutku@gmail.com>
2016-01-24 17:25:58 -0500

    Search Commit
    Working search function

--- js/app.js
+++ js/app.js
@@ -1,63 +1,133 @@
-//Place data
+//Place viewModel
 var places = [{
         "title": "Cookout",
         "description": "Cheapest fast food place in the area. The best choice when the fact of being broke becomes more urgent than eating well",
         "coordinates": {lat: 34.034523, lng: -84.571618},
-        "type": "restaurant"
+        "type": "Food"
     },
 
     {
         "title": "Cobb Center Mall",
         "description": "The nearest mall. Has standard mall stuff.",
         "coordinates": {lat: 34.017913, lng: -84.563803},
-        "type": "shopping"
+        "type": "Shopping"
     },
 
     {
         "title": "Kennesaw Mountain",
         "description": "The local 'mountain'. Place of a battle during the Civil War. Now a good hiking and picnic location for locals and college people going out on Sunday afternoons",
         "coordinates": {lat:  33.976374, lng: -84.579159},
-        "type": "sight"
+        "type": "Landmark"
     },
 
     {
         "title": "Lake Allatoona",
         "description": "The local lake. Good for swimming in the summer for people who are stuck in the vicinity or any reason",
         "coordinates": {lat:  34.133102, lng: -84.624366},
-        "type": "sight"
+        "type": "Landmark"
     },
 
     {
         "title": "Kennesaw State University",
         "description": "The local university. I go here.",
         "coordinates": {lat: 34.038296, lng: -84.583061},
-        "type": "school"
+        "type": "School"
     },
 ];
 
+//Global variables
+var map;
+var mapMarkers = [];
 
-//Marker Model
-MarkerModel = function(places) {
-    console.log("this");
-    var self = this;
-    self.places = ko.observableArray();
+//Initialize markers
+function MarkerInit(place) {
+    this.title = place.title;
+    this.cords = place.coordinates;
+    this.desc = place.description;
+    this.type = place.type;
 
-    console.log("or this");
-    for (var i = 0; i < json.length; i ++) {
-        var item = new MarkerModel(places[i]);
-        self.places.push(item);
+    function infoWindowContent(place){
+        var contentString = "<h3>"+this.type+"</h3>"+
+            "<h4>" + this.title + "</h4>"+
+            "<p>"+this.desc+"</p>";
+        return contentString;
     }
+    var infoWindow = new google.maps.InfoWindow({
+        content: infoWindowContent(place)
+    });
 
-    console.log(self.places);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.cords),
+        title: this.title,
+        label: this.type,
+        map: map
+    });
+
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
+
+    mapMarkers.push(marker);
+
 }
 
 //Render Map
-
 function initMap() {
-    var map;
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat:  33.989474, lng: -84.537597},
         zoom: 13
     });
+
+    //Initialize markers
+
+    for (i in places){
+            MarkerInit(places[i]);
+    };
+
+    console.log(mapMarkers);
 }
 
+//Search ViewModel
+var SearchViewModel = function () {
+    this.phraseToSearch = ko.observable("");
+
+    this.allCategories = ko.observableArray([]); //All searchable items
+    for (i in places){
+        if (this.allCategories.indexOf(places[i].type) < 0) {
+            this.allCategories.push(places[i].type);
+        }
+
+    }
+
+    this.categoriesToSearch = ko.observableArray(["Food"]);// Initial selection since everyone loves food
+
+
+    this.searchByPhrase = function () {
+        var markerVisibility;
+        for (i in mapMarkers){
+            markerVisibility = true;
+            if (mapMarkers[i].title.toLowerCase().match(this.phraseToSearch().toLowerCase()) == null){
+                markerVisibility = false;
+            };
+            mapMarkers[i].setVisible(markerVisibility);
+        }
+    };
+
+    this.searchByCategories = function () {
+        var markerVisibility;
+        for (i in mapMarkers){
+            markerVisibility = false;
+            for (j in this.categoriesToSearch()){
+                if (this.categoriesToSearch()[j] == mapMarkers[i].label){
+                    markerVisibility = true;
+                }
+                else{
+                   continue;
+                }
+            }
+            mapMarkers[i].setVisible(markerVisibility);
+        }
+    };
+};
+
+ko.applyBindings(new SearchViewModel());
